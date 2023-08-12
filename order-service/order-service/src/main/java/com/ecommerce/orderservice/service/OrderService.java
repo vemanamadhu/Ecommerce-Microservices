3881d11778fbc55c5f3a980107fbdc088efea02c
MadhuV <f2013152@goa.bits-pilani.ac.in>
2023-08-12 17:38:20 -0400

    Added service to do the business logic of creating new order

---
+++ order-service/order-service/src/main/java/com/ecommerce/orderservice/service/OrderService.java
@@ -0,0 +1,45 @@
+package com.ecommerce.orderservice.service;
+
+import com.ecommerce.orderservice.dto.OrderLineItemsDto;
+import com.ecommerce.orderservice.dto.OrderRequest;
+import com.ecommerce.orderservice.model.Order;
+import com.ecommerce.orderservice.model.OrderLineItems;
+import com.ecommerce.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest){
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        Order order = Order.builder()
+                .orderNumber(UUID.randomUUID().toString())
+                .orderLineItemsList(orderLineItemsList)
+                .build();
+
+        orderRepository.save(order);
+
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+    return OrderLineItems.builder()
+            .id(orderLineItemsDto.getId())
+            .skuCode(orderLineItemsDto.getSkuCode())
+            .price(orderLineItemsDto.getPrice())
+            .quantity(orderLineItemsDto.getQuantity())
+            .build();
+
+
+    }
+}
