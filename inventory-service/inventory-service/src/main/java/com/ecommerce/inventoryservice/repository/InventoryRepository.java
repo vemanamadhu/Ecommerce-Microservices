6279f334c3569d1c74dfe9eeaf1aa76da8c38cce
MadhuV <f2013152@goa.bits-pilani.ac.in>
2023-08-12 19:22:37 -0400

    Added repository for inventory service

---
+++ inventory-service/inventory-service/src/main/java/com/ecommerce/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.inventoryservice.repository;
+
+import com.ecommerce.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode();
+}
