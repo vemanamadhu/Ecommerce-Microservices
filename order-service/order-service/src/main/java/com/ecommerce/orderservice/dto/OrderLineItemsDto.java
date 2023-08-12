a71e9b5056a751e188f1a28492ec58134856dade
MadhuV <f2013152@goa.bits-pilani.ac.in>
2023-08-12 16:34:25 -0400

    Added order service DTOs orderrequest and orderlineitems

---
+++ order-service/order-service/src/main/java/com/ecommerce/orderservice/dto/OrderLineItemsDto.java
@@ -0,0 +1,18 @@
+package com.ecommerce.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItemsDto {
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private int quantity;
+}
