675392496f63bb9680417144924ec30af3babf74
MadhuV <f2013152@goa.bits-pilani.ac.in>
2023-08-12 19:47:29 -0400

    Adding inventory service files

--- inventory-service/inventory-service/src/main/java/com/ecommerce/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/inventory-service/src/main/java/com/ecommerce/inventoryservice/InventoryServiceApplication.java
@@ -2,12 +2,13 @@ package com.ecommerce.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
-
 }
