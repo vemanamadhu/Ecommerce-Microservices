69d30c6cf73b6153263fd707c420da868f2d093e
MadhuV <f2013152@goa.bits-pilani.ac.in>
2023-08-12 19:22:59 -0400

    Added appication properties

--- inventory-service/inventory-service/src/main/java/com/ecommerce/inventoryservice/service/InventoryService.java
+++ inventory-service/inventory-service/src/main/java/com/ecommerce/inventoryservice/service/InventoryService.java
@@ -1,2 +1,16 @@
-package com.ecommerce.inventoryservice.service;public class InventoryService {
+package com.ecommerce.inventoryservice.service;
+
+import com.ecommerce.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode){
+        return inventoryRepository.findBySkuCode().isPresent();
+    }
 }
