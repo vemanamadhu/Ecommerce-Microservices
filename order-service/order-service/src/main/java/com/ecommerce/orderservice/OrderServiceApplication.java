3555432a6779e9d26545b2859db1a8fbae01d140
MadhuV <f2013152@goa.bits-pilani.ac.in>
2023-08-12 17:40:17 -0400

    Added Enable MVC annotation to expose order service

--- order-service/order-service/src/main/java/com/ecommerce/orderservice/OrderServiceApplication.java
+++ order-service/order-service/src/main/java/com/ecommerce/orderservice/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
