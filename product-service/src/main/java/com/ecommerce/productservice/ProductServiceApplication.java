43724dbf04a6789a286020c2d86b2ffd72934388
MadhuV <f2013152@goa.bits-pilani.ac.in>
2023-08-22 02:10:22 -0400

    Added eureka client in product service

--- product-service/src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
@@ -2,12 +2,12 @@ package com.ecommerce.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-// @ComponentScan(basePackages = "com.ecommerce")
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
