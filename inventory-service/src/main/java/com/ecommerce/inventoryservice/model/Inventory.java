614ed3147a6d0aee903725b987f70cfb402658ee
MadhuV <f2013152@goa.bits-pilani.ac.in>
2023-08-22 02:08:28 -0400

    Added Inventory response and created corresponding controller and service

--- inventory-service/src/main/java/com/ecommerce/inventoryservice/model/Inventory.java
+++ inventory-service/src/main/java/com/ecommerce/inventoryservice/model/Inventory.java
@@ -1,13 +1,12 @@
 package com.ecommerce.inventoryservice.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name="t_inventory")
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Inventory {
